e82cb8767b582bfafdeb126c940186e9b99714c4
CCabricot <81744905+CCabricot@users.noreply.github.com>
2021-09-29 23:12:52 +0200

    score list

--- app/src/main/java/com/example/myapplication/ScoreActivity.java
+++ app/src/main/java/com/example/myapplication/ScoreActivity.java
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -16,12 +17,13 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ScoreActivity extends AppCompatActivity {
-    //RecyclerView recyclerView;
+    RecyclerView recyclerView;
     TextView textView;
-    String[] scoreList;
+    ArrayList<String> scoreList = new ArrayList<String>();
     FirebaseFirestore db;
 
 
@@ -29,23 +31,27 @@ public class ScoreActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
-
-
-        //recyclerView = findViewById(R.id.recyclerviewScore);
-        //recyclerView.setHasFixedSize(true);
-        //recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        textView = findViewById(R.id.textView);
+        recyclerView = findViewById(R.id.recyclerview);
 
         db = FirebaseFirestore.getInstance();
         DocumentReference compte = db.collection("flapp").document("compte");
-        textView.setText("score");
-        compte.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+
+
+        compte.get().addOnSuccessListener(new OnSuccessListener <DocumentSnapshot>() {
             @Override
             public void onSuccess(DocumentSnapshot documentSnapshot) {
-                String score = documentSnapshot.getString("id");
-                textView.setText(score);
-                //scoreList = documentSnapshot.get("score");
-                //textView.setText(scoreList[0]);
+
+                // Recuperer les data de la liste "score" de firestore
+                scoreList = (ArrayList) documentSnapshot.get("score");
+
+                //Ordre decroissant
+                Collections.sort(scoreList);
+                Collections.reverse(scoreList);
+
+                textView.setText((String)scoreList.get(0));
+                //recyclerView.a;
+
+
 
             }
         });
@@ -53,3 +59,4 @@ public class ScoreActivity extends AppCompatActivity {
     }
 
 }
+
